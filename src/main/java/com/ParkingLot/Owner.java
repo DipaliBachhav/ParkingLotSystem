0026049062aad2590bafa3a5c9f8365892a6e018
DipaliBachhav <dipa2718@gmail.com>
2020-06-10 15:26:57 +0530

    Added Code For Owner Class

---
+++ src/main/java/com/ParkingLot/Owner.java
@@ -0,0 +1,13 @@
+package com.ParkingLot;
+
+public class Owner {
+    private String parkingFullOrNot;
+
+    public void setParkingFullOrNot(String parkingFullOrNot) {
+        this.parkingFullOrNot = parkingFullOrNot;
+    }
+
+    public String getParkingFullOrNot() {
+        return parkingFullOrNot;
+    }
+}
