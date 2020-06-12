16c028789160e2875e1cde098fcd0534eae56e61
DipaliBachhav <dipa2718@gmail.com>
2020-06-12 21:56:23 +0530

    Added Set Method To Set Parking Charges

--- src/main/java/com/ParkingLot/Owner.java
+++ src/main/java/com/ParkingLot/Owner.java
@@ -2,9 +2,14 @@ package com.ParkingLot;
 
 public class Owner {
     private String parkingFullOrNot;
+    private String parkingCharge;
 
     public void setParkingFullOrNot(String parkingFullOrNot) {
         this.parkingFullOrNot = parkingFullOrNot;
+    }
+
+    public void setParkingCharge(String parkingCharge) {
+        this.parkingCharge = parkingCharge;
     }
 
     public String getParkingFullOrNot() {
